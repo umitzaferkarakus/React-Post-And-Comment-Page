1de493eba85181b09ce7cd72076c97e2ac77c910
Zafer Karakus <zafer.umut97@gmail.com>
2022-08-18 11:42:12 +0300

    Post Commit Added

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,20 +2,54 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
+const COMMENT_START = {
+  display_name: "",
+  body: "",
+};
+
 const PostDetail = (props) => {
   const { id } = useParams();
   const [postDetail, setPostDetail] = useState({});
+  const [postComment, setPostComment] = useState([]);
+  const [commentBody, setCommentBody] = useState(COMMENT_START);
 
-  useEffect(() => {
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
     axios
-      .get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
+      .post(
+        `https://react-yazi-yorum.herokuapp.com/posts/${id}/comments`,
+        commentBody
+      )
       .then((response) => {
-        setPostDetail(response.data);
+        setPostComment([...postComment, response.data]);
+        setCommentBody(COMMENT_START);
       })
       .catch((error) => {
         console.log(error);
       });
-  });
+  };
+
+  const handleOnChange = (event) => {
+    setCommentBody({ ...commentBody, [event.target.name]: event.target.value });
+  };
+
+  useEffect(() => {
+    axios
+      .all([
+        axios.get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`),
+        axios.get(
+          `https://react-yazi-yorum.herokuapp.com/posts/${id}/comments`
+        ),
+      ])
+      .then((responses) => {
+        console.log(responses);
+        setPostDetail(responses[0].data);
+        setPostComment(responses[1].data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
@@ -23,6 +57,52 @@ const PostDetail = (props) => {
         <h2 className="ui header">{postDetail.title}</h2>
         <p>{postDetail.content} </p>
         <p>{postDetail.created_at} </p>
+
+        <h3>Comments</h3>
+        {postComment.map((comment) => {
+          return (
+            <div className="ui relaxed list" key={comment.id}>
+              <div className="item">
+                <img
+                  className="ui avatar image"
+                  src="/images/avatar/small/daniel.jpg"
+                />
+                <div className="content">
+                  <a className="header">{comment.display_name} </a>
+                  <div className="description">{comment.body} </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+
+        <h3>Write Your Comment</h3>
+        <form
+          className="ui form"
+          onSubmit={handleCommentSubmit}
+        >
+          <div className="ui mini icon input">
+            <input
+              name="display_name"
+              type="text"
+              placeholder="Username"
+              onChange={handleOnChange}
+              value={commentBody.display_name}
+            />
+          </div>
+          <textarea
+            name="body"
+            placeholder="Write Your Comment"
+            rows="3"
+            onChange={handleOnChange}
+            value={commentBody.body}
+          ></textarea>
+          <div>
+            <button className="ui blue button" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </React.Fragment>
     </div>
   );
